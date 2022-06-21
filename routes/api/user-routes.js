3f17986b878cd652f56b8f3e9845736572d57d39
Leah <amelia_rawls@ccpsnet.net>
2022-06-21 16:14:11 -0400

    updated routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  removeUser,
+  addFriend,
+  removeFriend
+} = require('../../controllers/user-controller');
+
+// /api/users
+router
+  .route('/')
+  .get(getAllUsers)
+  .post(createUser);
+
+// /api/users/:id
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(removeUser);
+
+  // /api/users/:userId/friends/:friendId
+  router
+   .route(':id/friends/:friendId')
+   .post(addFriend)
+   .delete(removeFriend)
+
+module.exports = router;
